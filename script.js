f015f973804c1d50a6c648d029bc253098e4089e
Kareem Alsayd <106820544+kemooalsayd@users.noreply.github.com>
2022-06-29 15:37:38 +0200

    sharing the video
    you can share a video in chat app

--- script.js
+++ script.js
@@ -6,12 +6,32 @@ let chat=document.getElementById("ul");
 let inp=document.getElementById("input");
 let send=document.getElementById("send");
 let tit=document.getElementById("tit");
-lll=document.querySelectorAll("#ul li .massage")
-
+let imgt=document.getElementById("img-t");
+let img=document.getElementById("img");
+let video=document.getElementById("video");
+let vt=document.getElementById("video-t");
+let btn=document.getElementById("btn");
+btn.onclick=(e)=>{
+  e.preventDefault()
+}
+function t(e,w){
+  e.onclick=()=>{
+    
+    w.click()
+  }
+}
+t(imgt,img)
+t(vt,video)
 tit.innerText="kareem"
 bar.onclick=()=>{
     side.classList.toggle("y")
 }
+
+
+
+
+
+
 o= new Date()
 time=o.getHours()+":"+o.getMinutes()
 send.onclick=(e)=>{
@@ -19,7 +39,6 @@ send.onclick=(e)=>{
   
 
  
-    
 
 
  if(inp.value.trim()!=""){
@@ -54,33 +73,56 @@ mo.onclick=()=>{
   chat.classList.add("boy")
   document.documentElement.style.setProperty("--my","#80cf61")
   document.documentElement.style.setProperty("--mu","#7bb6ed")
-function previewImage() {
-  
-  var file = document.getElementById("file").files;
-  if (file.length > 0) {
-      var fileReader = new FileReader();
-      fileReader.onload = function (event) {
-         lo=`<img src="${event.target.result}"`
-          img=`
-         
-<li>
-<div class="message-data g">
-  <span class="message-data-name">mohamed</span>
-  <span class="message-data-time">${time},Today</span>
-</div>
-<div class="message my-message">
-${lo}
-</div>
-</li>`
+  function previeImage() {
+    var imtg = document.getElementById("img").files;
+    if (imtg.length > 0) {
+        var fileReader = new FileReader();
+        fileReader.onload = function (event) {
+           lp=`<img src="${event.target.result}">`
+           //lm=`<video controls src="${event.target.result}"></video>`
+            imgm=`
+            <li>
+      <div class="message-data g">
+        <span class="message-data-name">mohamed</span>
+        <span class="message-data-time">${time},Today</span>
+      </div>
+      <div class="message my-message">
+      ${lp}
+      </div>
+    </li>`
+          chat.innerHTML+=imgm
+        };
+        fileReader.readAsDataURL(imtg[0]);
+      }}
 
-        chat.innerHTML+=img
-      };
-      fileReader.readAsDataURL(file[0]);
-    }}
-    file.onchange=()=>{
-      previewImage()
-    }
-  
+      function previeVideo() {
+        var imop = document.getElementById("video").files;
+        if (imop.length > 0) {
+            var fileReader = new FileReader();
+            fileReader.onload = function (event) {
+               lom=`<video controls src="${event.target.result}"></video>`
+               //lm=`<video controls src="${event.target.result}"></video>`
+                imgt=`
+                <li>
+      <div class="message-data g">
+        <span class="message-data-name">mohamed</span>
+        <span class="message-data-time">${time},Today</span>
+      </div>
+      <div class="message my-message">
+      ${lom}
+      </div>
+    </li>`
+              chat.innerHTML+=imgt
+            };
+            fileReader.readAsDataURL(imop[0]);
+          }}
+          img.onchange=()=>{
+            previeImage()
+          }
+          video.onchange=()=>{
+            previeVideo()
+          }
+     
 send.onclick=(e)=>{
   e.preventDefault()
  if(inp.value.trim()!=""){
@@ -111,8 +153,69 @@ ka.onclick=()=>{
   chat.classList.remove("boy")
   document.documentElement.style.setProperty("--my","#7bb6ed")
   document.documentElement.style.setProperty("--mu","#80cf61")
-  file.onchange=()=>{
+
+
+
+
+
+
+  
+  function previewImage() {
+    var im = document.getElementById("img").files;
+    if (im.length > 0) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (event) {
+         lo=`<img src="${event.target.result}">`
+         //lm=`<video controls src="${event.target.result}"></video>`
+          img=`
+          <li >
+                <div  class="message-data  align-right">
+                <span class="message-data-time" >${time},Today</span> &nbsp; &nbsp;
+                <span class="message-data-name" >kareem</span>
+                </div>
+                <div  class="message other-message float-right">
+                 ${lo}
+                </div>
+              </li>`
+        chat.innerHTML+=img
+      };
+      fileReader.readAsDataURL(im[0]);
+    }}
+      
+     function previewVideo() {
+      var im = document.getElementById("video").files;
+      if (im.length > 0) {
+          var fileReader = new FileReader();
+          fileReader.onload = function (event) {
+             lo=`<video controls src="${event.target.result}"></video>`
+             //lm=`<video controls src="${event.target.result}"></video>`
+              img=`
+              <li >
+                    <div  class="message-data  align-right">
+                    <span class="message-data-time" >${time},Today</span> &nbsp; &nbsp;
+                    <span class="message-data-name" >kareem</span>
+                    </div>
+                    <div  class="message other-message float-right">
+                     ${lo}
+                    </div>
+                  </li>`
+            chat.innerHTML+=img
+          };
+          fileReader.readAsDataURL(im[0]);
+        }}
+    
+
+
+
+
+
+
+
+  img.onchange=()=>{
     previewImage()
+  }
+  video.onchange=()=>{
+    previewVideo()
   }
   send.onclick=(e)=>{
     e.preventDefault()
@@ -139,24 +242,49 @@ ka.onclick=()=>{
   }
   }
  
-  function previewImage() {
-    var file = document.getElementById("file").files;
-    if (file.length > 0) {
-        var fileReader = new FileReader();
-        fileReader.onload = function (event) {
-           lo=`<img src="${event.target.result}"`
-            img=`
-            <li >
-                  <div  class="message-data  align-right">
-                  <span class="message-data-time" >${time},Today</span> &nbsp; &nbsp;
-                  <span class="message-data-name" >kareem</span>
-                  </div>
-                  <div  class="message other-message float-right">
-                   ${lo}
-                  </div>
-                </li>`
-          chat.innerHTML+=img
-        };
-        fileReader.readAsDataURL(file[0]);
-      }}
-     
+ 
+    
+  
+   function previewImage() {
+var im = document.getElementById("img").files;
+if (im.length > 0) {
+  var fileReader = new FileReader();
+  fileReader.onload = function (event) {
+     lo=`<img src="${event.target.result}">`
+     //lm=`<video controls src="${event.target.result}"></video>`
+      img=`
+      <li >
+            <div  class="message-data  align-right">
+            <span class="message-data-time" >${time},Today</span> &nbsp; &nbsp;
+            <span class="message-data-name" >kareem</span>
+            </div>
+            <div  class="message other-message float-right">
+             ${lo}
+            </div>
+          </li>`
+    chat.innerHTML+=img
+  };
+  fileReader.readAsDataURL(im[0]);
+}}
+  
+ function previewVideo() {
+  var im = document.getElementById("video").files;
+  if (im.length > 0) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (event) {
+         lo=`<video controls src="${event.target.result}"></video>`
+         //lm=`<video controls src="${event.target.result}"></video>`
+          img=`
+          <li >
+                <div  class="message-data  align-right">
+                <span class="message-data-time" >${time},Today</span> &nbsp; &nbsp;
+                <span class="message-data-name" >kareem</span>
+                </div>
+                <div  class="message other-message float-right">
+                 ${lo}
+                </div>
+              </li>`
+        chat.innerHTML+=img
+      };
+      fileReader.readAsDataURL(im[0]);
+    }}
